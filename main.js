6da77df5ee51bb3da03a95c2280b76b14329ee84
t9957 <t995771take@gmail.com>
2025-11-22 05:33:25 +0900

    del dev code

--- main.js
+++ main.js
@@ -4,11 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const timersContainer = document.querySelector('.timer-container');
   const addTimerButton = document.getElementById('addTimer');
   addTimerButton.value = 1;
-  const timer = new Timer(timersContainer);
-  /* Dev */
-  document.querySelector('.timer-display').value = 15;
-  timer.play();
-  /* end */
+  new Timer(timersContainer);
   addTimerButton.addEventListener('click', function () {
     new Timer(timersContainer);
   });
